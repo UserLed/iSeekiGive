4c1a4e131523f41c2a4c7a2c83a50c37edf323ce
Iconsbd <iconsbd@gmail.com>
2013-09-02 17:30:36 +0600

    popup on user show page

--- app/assets/javascripts/fancybox-config.js
+++ app/assets/javascripts/fancybox-config.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $("a.fancybox").fancybox();
+    $("a.fancybox").fancybox({
+        });
+
+    $("a.dashboard_popup").fancybox({
+        autoDimensions: false,
+        height: 303,
+        width: 400
+    });
+    $('.dashboard_popup').click();
 });
