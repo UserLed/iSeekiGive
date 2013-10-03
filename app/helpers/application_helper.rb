90b18c904277e1d545fef391bafc598bda8fe240
farhantanvir <farhan@iconsbd.com>
2013-10-03 18:09:17 +0600

    fixing game story saving and experience calculate method content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,6 @@
 module ApplicationHelper
 
   def total_experience(user)
-    total_time = 0
-    
-    user.experiences.each do |e|
-      if e.start_date.present?
-        from_time = e.start_date.to_time
-        to_time = (e.end_date or Date.today).to_time
-        distance_in_seconds = ((to_time - from_time).abs).round
-        total_time = distance_in_seconds + total_time
-      end
-    end
-
-    components = []
-
-    %w(year month).each do |interval|
-      if total_time >= 1.send(interval)
-        delta = (total_time / 1.send(interval)).floor
-        total_time -= delta.send(interval)
-        components << pluralize(delta, interval)
-      end
-    end
-    components.join(" ")
-  end
-
-  def new_total_exp(user)
     exp_arr = []
     user_exps = user.experiences.collect{|x| [x.start_date, (x.end_date || Date.today)]}.sort
     tmp_arr = []
