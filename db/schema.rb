fafcc486985e93539504be3740715394870ac0ac
farhantanvir <farhan@iconsbd.com>
2013-10-09 16:55:58 +0600

    fixing perspectives js isues

--- db/schema.rb
+++ db/schema.rb
@@ -125,6 +125,13 @@ ActiveRecord::Schema.define(:version => 20131009084352) do
     t.datetime "updated_at",                           :null => false
   end
 
+  create_table "keywords", :force => true do |t|
+    t.text     "story_keyword"
+    t.integer  "game_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
   create_table "messages", :force => true do |t|
     t.string   "from"
     t.string   "to"
@@ -184,6 +191,14 @@ ActiveRecord::Schema.define(:version => 20131009084352) do
     t.boolean  "status",     :default => true
     t.datetime "created_at",                   :null => false
     t.datetime "updated_at",                   :null => false
+  end
+
+  create_table "save_game_tags", :force => true do |t|
+    t.string   "tag_name"
+    t.string   "experience_name"
+    t.integer  "user_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "schedules", :force => true do |t|
