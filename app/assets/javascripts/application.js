ff0423b488e5edd661fc0d8aa86c6414e0fb9f00
Shahjahan <shahjahan@iconsbd.com>
2013-10-11 17:02:01 +0600

    user testimonial font size make small

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,13 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require tag-it
+//= require underscore-min
+//= require readmore
 //= require twitter/bootstrap
 //= require fancybox
 //= require underscore-min
