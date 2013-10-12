37b1eb6e00cbceb75d7e630171c859d8147323e2
Shahjahan <shahjahan@iconsbd.com>
2013-10-12 15:51:04 +0600

    password changes implemented

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,17 +1,18 @@
 class AccountsController < UsersController
+  before_filter :require_login
+
+  def index
+
+  end
 
   def settings
     @user = current_user
-    logger.info "Current User :: #{@user.inspect}"
-    logger.info "params :: #{params.inspect}"
     if request.post?
-      logger.info "Post Req==="
       @user.password_confirmation = params[:user][:password_confirmation]
-      # the next line clears the temporary token and updates the password
       if @user.change_password!(params[:user][:password])
-        redirect_to(root_path, :notice => 'Password was successfully updated.')
+        redirect_to(accounts_path, :notice => 'Password was successfully updated.')
       else
-        logger.info "Password not reset"
+
       end
     end
   end
